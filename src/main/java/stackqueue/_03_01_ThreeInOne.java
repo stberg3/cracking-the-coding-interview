6e8603bb7ade3b4758725687709b5d543d389d0f
Sam <stberg3@gmail.com>
2021-02-27 21:12:37 -0600

    3.1 Done with dumb test failure

--- src/main/java/stackqueue/_03_01_ThreeInOne.java
+++ src/main/java/stackqueue/_03_01_ThreeInOne.java
@@ -5,18 +5,41 @@ package stackqueue;
  */
 class _03_01_ThreeInOne {
 
+    int[] idx;
+    int space;
+    int[] arr;
 
     _03_01_ThreeInOne(int initSize) {
+        this.arr = new int[initSize*3];
+        this.space = initSize;
+        this.idx = new int[3];
+    }
 
+    private void expand() {
+        int[] newArr = new int[arr.length * 2];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < space; j++) {
+                newArr[i * 2 * space + j] = arr[i * space + j];
+            }
+        }
+
+        space *= 2;
+        arr = newArr;
     }
 
     void push(int stackNum, int val) {
-        throw new UnsupportedOperationException();
+        if(idx[stackNum] >= space) { expand(); }
+
+        int index = (stackNum * space) + idx[stackNum];
+        arr[index] = val;
+        idx[stackNum]++;
     }
 
     int pop(int stackNum) {
-        throw new UnsupportedOperationException();
+        idx[stackNum]--;
+        int index = (stackNum * space) + idx[stackNum];
+        int res = arr[index];
+        return res;
     }
-
-
 }
